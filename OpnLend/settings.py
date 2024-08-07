0b08127ab8a4006e86da495ff123bfc18a428d9a
Rustam Ansari <rustamansari.omnisttechhub@gmail.com>
2024-08-08 01:28:54 +0530

    Add whitenoise to requirements and update settings

--- OpnLend/settings.py
+++ OpnLend/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
+# Update this to get the port from the environment
+PORT = int(os.environ.get('PORT', 8000))
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
